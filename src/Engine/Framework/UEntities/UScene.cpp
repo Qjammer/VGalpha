39d6a4a70062c109df56ed8cadd6a255a0e41348
Qjammer <baladoramiro+github@gmail.com>
2015-10-13 11:15:36 +0200

    fixed stupid errors

--- src/Engine/Framework/UEntities/UScene.cpp
+++ src/Engine/Framework/UEntities/UScene.cpp
@@ -16,10 +16,10 @@ void UScene::unregisterExtension(const std::shared_ptr<SceneExtension>& ptr){
 	this->extensions_.remove(ptr);
 }
 
-void UScene::registerUObject(const std::shared_ptr<UObject>&){
-	this->uObjects.push_back(ptr);
+void UScene::registerUObject(const std::shared_ptr<UObject>& ptr){
+	this->uObjects_.push_back(ptr);
 }
 
-void UScene::unregisterUObject(const std::shared_ptr<UObject>&){
-	this->uObjects.remove(ptr);
+void UScene::unregisterUObject(const std::shared_ptr<UObject>& ptr){
+	this->uObjects_.remove(ptr);
 }
--- src/Engine/Framework/UEntities/UScene.hpp
+++ src/Engine/Framework/UEntities/UScene.hpp
@@ -13,7 +13,7 @@ public:
 	void unregisterExtension(const std::shared_ptr<SceneExtension>&);
 
 	void registerUObject(const std::shared_ptr<UObject>&);
-	void registerUObject(const std::shared_ptr<UObject>&);
+	void unregisterUObject(const std::shared_ptr<UObject>&);
 	
 private:
 	std::list<std::shared_ptr<UObject>> uObjects_;
