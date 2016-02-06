2a5327bd5690f8f399a573de0bc56b9dce725473
Qjammer <baladoramiro+github@gmail.com>
2016-02-06 12:55:07 +0100

    Revised logging process

--- src/Utilities/Logger.cpp
+++ src/Utilities/Logger.cpp
@@ -2,7 +2,7 @@
 
 Logger::Logger(std::string _fileDir,bool _logStatus,bool _consoleStatus,std::weak_ptr<std::stringstream> _console):
 	fileOutputDir_(_fileDir),
-	fileOutputStream_(_fileDir,std::ofstream::out|std::ofstream::app),
+	fileOutputStream_(_fileDir,std::ofstream::out | std::ofstream::app),
 	date_(std::string()),
 	logStatus_(_logStatus),
 	consoleStatus_(_consoleStatus),
@@ -11,6 +11,7 @@ Logger::Logger(std::string _fileDir,bool _logStatus,bool _consoleStatus,std::wea
 {
 
 }
+
 std::shared_ptr<std::stringstream> consoleStream(new std::stringstream());//TODO:  THis is temporary, as soon as we get a console it should construct from there
 Logger::Logger():Logger(DEFAULT_LOGGER_OUTPUT,LOGGER_STATUS,CONSOLE_STATUS,consoleStream){
 
@@ -21,14 +22,13 @@ Logger::~Logger(){
 }
 
 void Logger::logString(std::string _message){
-	this->tempStringStream_.str(std::string());
-	this->tempStringStream_.clear();
-	this->tempStringStream_<<this->getTimestamp()<<" "<<_message<<std::endl;
+	std::string tstring(this->getTimestamp()+" "+_message+"\n");
 	if(this->logStatus_){
-		this->fileOutputStream_<<this->tempStringStream_.str();
+		this->fileOutputStream_<<tstring;
+		this->fileOutputStream_.flush();
 	}
 	if(this->consoleStatus_){
-		*(this->consoleStream_.lock())<<this->tempStringStream_.str();
+		*(this->consoleStream_.lock())<<tstring;
 	}
 }
 
@@ -44,6 +44,7 @@ void Logger::logMessage(std::string _message){
 	this->logString(_message);
 }
 
+//TODO:This function probably causes race conditions. Not incredibly important, since the result is still the same
 std::string Logger::getTimestamp(){
 	time(&(this->timer));
 	this->date_=ctime(&(this->timer));
