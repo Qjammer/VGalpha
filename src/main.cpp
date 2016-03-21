2befb13f5848a7d5a13029f5579c9a9efe853b58
Qjammer <baladoramiro+github@gmail.com>
2016-03-21 22:16:45 +0100

    cleaner main

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <cstdio>
 #include <chrono>
+#include <iostream>
 #include "./Systems/SystemInterface.hpp"
 #include "./Engine/Managers/TaskManagerInterface.hpp"
 #include "./Utilities/Logger.hpp"
@@ -29,15 +30,20 @@ public:
 int main(){
 
 	std::shared_ptr<TaskManagerInterface> tskmgr(std::make_shared<TaskManagerInterface>());
-	std::shared_ptr<SystemInterface> inter(std::make_shared<testSystemInterface>(tskmgr));
+	std::shared_ptr<SystemInterface> sysInter(std::make_shared<testSystemInterface>(tskmgr));
 	std::vector<std::weak_ptr<SystemInterface>> vect;
-	vect.push_back(std::weak_ptr<SystemInterface>(inter));
+	vect.push_back(std::weak_ptr<SystemInterface>(sysInter));
 
 	Scheduler testScheduler(std::weak_ptr<TaskManagerInterface>(tskmgr),vect);
 
-	for(int i=0;i<200;i++){
+	int cycles=200;
+
+	auto t1=std::chrono::system_clock::now();
+	for(int i=0;i<cycles;i++){
 		testScheduler.Execute();
 	}
+	auto t2=std::chrono::system_clock::now();
+	std::cout<<std::endl<<"Ideal:"<<(testScheduler.timePerTick_*cycles).count()<<" Real:"<<(t2-t1).count()<<std::endl;
 
 	tskmgr->getInstance()->wakeUpandStopAll();
 	tskmgr->getInstance()->joinThreads();
