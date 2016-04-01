1b276af948a2342c813ae02bc75b690717ce6f9a
Qjammer <baladoramiro+github@gmail.com>
2016-04-02 00:00:11 +0200

    added a few tasks to graphics

--- src/Systems/Graphics/GraphicSystem.cpp
+++ src/Systems/Graphics/GraphicSystem.cpp
@@ -12,9 +12,10 @@ GraphicSystem::~GraphicSystem(){
 }
 
 int GraphicSystem::mainTask(std::weak_ptr<TaskManagerInterface> _tskmgr){
+	printf("MnGfxTsk!");
 	std::list<std::function<int(void)>> lst;
 	lst.push_back([this](){	this->rwindow_.clear();return 0;});
-	// lst.push_back([this](){	this->rwindow_.clear();return 0;});
+	lst.push_back([](){printf("SecGfxTsk!");return 0;});
 	lst.push_back([this](){	this->rwindow_.display();return 0;});
 	_tskmgr.lock()->addTaskList(lst);
 	return 0;
