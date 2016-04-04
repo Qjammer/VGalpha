46a5bab15a2dd1996f202fa69450a68c30b4a199
Qjammer <baladoramiro+github@gmail.com>
2016-04-04 14:50:47 +0200

    fedging out the state manager

--- src/Engine/Managers/StateManager.cpp
+++ src/Engine/Managers/StateManager.cpp
@@ -1,5 +1,18 @@
 #include "./StateManager.hpp"
 
+BasicRequest::BasicRequest(void*_par):param_(_par){
+
+}
+
+Change::Change(void* _par,std::weak_ptr<void> _obj):param_(_par),obj_(_obj){
+
+}
+
+bool operator<(const Change _l, const Change _r){
+	return _l.param_<_r.param_;
+}
+
+
 StateManager::StateManager():
 	Manager(STATE)
 {
@@ -8,4 +21,33 @@ StateManager::StateManager():
 
 StateManager::~StateManager(){
 
+}
+
+void StateManager::addChange(void* _par,std::weak_ptr<void> _obj){
+	this->changeMap_.emplace(Change(_par,_obj),true);
+
+}
+
+void StateManager::addRequest(Change,std::weak_ptr<BasicRequest>){
+
+}
+
+void StateManager::checkValidity(){
+	for(auto it=this->requestMap_.begin();it!=this->requestMap_.end();++it){
+		if(it->first.obj_.use_count()==0){
+			this->requestMap_.erase(it--);//This moves the iterator back BEFORE deleting it, but returns a copy of the previous object
+		}
+	}
+}
+
+void StateManager::shareChanges(){
+	for(auto it=this->requestMap_.begin();it!=this->requestMap_.end();++it){
+		if(this->changeMap_.find(it->first)!=this->changeMap_.end()){//We don't need checking if the value is true, since it CAN ONLY BE TRUE
+			it->second->setData();
+		}
+	}
+}
+
+void StateManager::clearChanges(){
+	this->changeMap_.clear();
 }
--- src/Engine/Managers/StateManager.hpp
+++ src/Engine/Managers/StateManager.hpp
@@ -5,13 +5,10 @@
 
 #include "Manager.hpp"
 
-typedef void* Change;
-
 class BasicRequest{
 public:
-	BasicRequest(void* _par):param_(_par){}
+	BasicRequest(void*);
 	virtual void setData()=0;
-	void* getParamVoidPtr(){return this->param_;};
 	void* param_;
 };
 
@@ -26,11 +23,30 @@ public:
 
 };
 
+class Change{
+public:
+	Change(void*,std::weak_ptr<void>);
+	void* param_;
+	std::weak_ptr<void> obj_;
+};
+
+bool operator<(const Change _l, const Change _r);
+
 class StateManager: public Manager {
 public:
 	StateManager();
 	~StateManager();
+	void addChange(void*,std::weak_ptr<void>);
+	void addRequest(Change,std::weak_ptr<BasicRequest>)
 
-	std::multimap<Change,std::shared_ptr<BasicRequest>> changeMap_;
+	void checkValidity();
+	void shareChanges();
+
+	void clearChanges();
+
+
+protected:
+	std::multimap<Change,std::shared_ptr<BasicRequest>> requestMap_;
+	std::map<Change,bool> changeMap_;
 
 };
